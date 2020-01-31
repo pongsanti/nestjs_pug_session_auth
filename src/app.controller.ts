199ca0b49ec087758576e39612a4c371323a6547
santi tan <pongsanti.tanvejsilp@gmail.com>
2020-01-31 11:27:39 +0700

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+
+  @Get()
+  @Render('index')
+  root() {
+    return {
+      pageTitle: 'Pug test',
+      youAreUsingPug: true,
+    };
+  }
+}
