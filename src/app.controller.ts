1bef0eac967b64969de614df27febdfb03b75fcc
santi tan <pongsanti.tanvejsilp@gmail.com>
2020-01-31 12:20:45 +0700

    passport local strategy for username password auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,10 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Request, Post, UseGuards, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
-
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
 
   @Get()
   @Render('index')
@@ -16,6 +12,17 @@ export class AppController {
     return {
       pageTitle: 'Pug test',
       youAreUsingPug: true,
+    };
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  @Render('profile')
+  async login(
+    @Request() req,
+  ) {
+    return {
+      user: req.user,
     };
   }
 }
